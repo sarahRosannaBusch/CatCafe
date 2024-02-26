436d66ef42295acb3cad26f933cde71e9857f71b
sarahRosannaBusch <annarose0113@gmail.com>
2024-02-26 12:39:02 -0800

    initial commit

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Header />
+      <Cafe />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +15,55 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#444',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
+const Header = () => {
+  return (
+    <View>
+      {/* <Menu /> */}
+      <Text>Ar0se's Cat Cafe</Text>
+      {/* <Clock /> */}
+    </View>
+  )
+}
+
+const Cafe = () => {
+  return (
+    <View>
+      <Cat name="Maru" />
+      <Cat name="Jellylorum" />
+      <Cat name="Munkustrap" />
+      <Cat name="Spot" />
+    </View>
+  );
+};
+
+const Cat = props => {
+  const [isHungry, setIsHungry] = useState(true);
+  const [timesPetted, setTimesPetted] = useState(0)
+
+  return (
+    <View>
+      <Text>Hello, I am {props.name}!</Text>
+      <Text>I am {isHungry ? 'hungry' : 'full'}!</Text>
+      <Text>I've been pet {timesPetted} times</Text>
+      <Button
+        onPress={() => {
+          setIsHungry(false);
+        }}
+        disabled={!isHungry}
+        title={isHungry ? 'Pour milk' : 'Thank you!'}
+      />
+      <Button
+        onPress={() => {
+          setTimesPetted(timesPetted + 1);
+        }}
+        title={'Pet cat'}
+      />
+    </View>
+  );
+};
